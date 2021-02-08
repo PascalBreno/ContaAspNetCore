d9966a047fcb367acf2810aef638e3ab354c9e80
breno <brenodml@live.com>
2021-02-08 02:46:27 -0400

    Adiciona verificação para Multa na service

--- Domain/Service/ContaService.cs
+++ Domain/Service/ContaService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Domain.Entities;
 using Domain.Interfaces.Repositories.Base;
 using Domain.Interfaces.Services;
@@ -12,7 +13,27 @@ namespace Domain.Service
 
         public ContaService(IValidator<Conta> validator, IRepository<Conta> repository) : base(validator, repository)
         {
+            
         }
-        
+
+        public async Task<Conta> Add(Conta obj)
+        {
+            if (obj.DataPagamento <= obj.DataVencimento) return await base.Add(obj);
+            var DiasDeAtraso = (obj.DataPagamento - obj.DataVencimento).TotalDays;
+            if (DiasDeAtraso <= 3)
+            {
+                //Cndição para multa de 2% e juros de 0,1% ao dia
+            }
+            else if (DiasDeAtraso > 3 && DiasDeAtraso <= 5)
+            {
+                //Condição para multa de 3% e juros de 0,2% ao dia
+            }
+            else if (DiasDeAtraso > 5)
+            {
+                //Condição para multa de 3% e juros de 0,3% ao dia
+            }
+
+            return await base.Add(obj);
+        }
     }
 }
